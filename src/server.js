570f137a5f0f72759a30e59e0b5f750b71b0531e
shivam yadav <shivamyadav993671@gmail.com>
2026-08-31 17:25:48 +0530

    add standalone src/server.js and src/package.json for Render

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-import "../server.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "../.env"
+});
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`⚙️  Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed !!! ", err);
+  });

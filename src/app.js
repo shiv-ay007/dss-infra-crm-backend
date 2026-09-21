33319bb88610f2ec6071b60e1654f777d3a609e9
shivam yadav <shivamyadav993671@gmail.com>
2026-09-21 18:50:27 +0530

    cors error

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(
       "http://localhost:5173",
       "https://crm.dssinfra.in",
       "http://crm.dssinfra.in",
-      "https://dss-infra-crm.vercel.app/"
+      "https://dss-infra-crm.vercel.app"
     ],
     credentials: true
   })
